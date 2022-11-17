735ecb3250623b479a149cd8b21ae830afcba77d
unknown <buildforfb@gmail.com>
2022-11-17 13:02:07 +0530

    request limit added of 10mb

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cron = require("node-cron");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 const todo = [];
 const ongoing = [];
