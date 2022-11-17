bf9c03cf477fb23d2abbc23afa0584f6a5fac1f6
unknown <buildforfb@gmail.com>
2022-11-17 14:21:08 +0530

    minor change

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cron = require("node-cron");
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 
 const todo = [];
 const ongoing = [];
